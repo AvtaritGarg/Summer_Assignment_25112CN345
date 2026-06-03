7339546dbcaf2c6e23679b78ce30ab7a5c29966c
Avtarit Garg <avtaritgarg05@gmail.com>
2026-06-03 17:13:47 +0530

    Day-5 uploaded.

--- Day_5/20.c
+++ Day_5/20.c
@@ -9,9 +9,10 @@ int main(){
     scanf("%d", &n);
 
     for(i=2;i<=n;i++){
-        if(n%i==0) for(j=2;j<=i/2;j++){
+        prime = 0;
+        if(n%i==0){ for(j=2;j<=i/2;j++){
             if(i%j==0) prime++;
-        } if(prime==0) largestPrime = i;
+        } if(prime==0) largestPrime = i;}
     }
     printf("Largest prime factor = %d\n", largestPrime);
 }
